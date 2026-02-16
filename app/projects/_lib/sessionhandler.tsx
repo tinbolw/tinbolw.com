6d767a17c8999dd66d449f670d255f835456b2ad
Thomas Van <tinbolw@gmail.com>
2026-02-15 21:20:40 -0800

    move async actions out of projects page

--- app/projects/_lib/sessionhandler.tsx
+++ app/projects/_lib/sessionhandler.tsx
@@ -1,6 +1,5 @@
 "use server";
 
-import { authClient } from "@/app/_lib/auth-client"
 import { auth } from "@/app/_lib/auth";
 import { headers } from "next/headers";
 
@@ -9,16 +8,4 @@ export async function getSession() {
         headers: await headers()
     })
     return session;
-}
-
-export async function login() {
-    await authClient.signIn.social({
-        provider: "google",
-        callbackURL: "/projects",
-        errorCallbackURL: "/projects",
-    })
-}
-
-export async function logout() {
-    await authClient.signOut();
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,13 +4,9 @@ import StyledLink from '../_components/styledlink';
 import CardRow from './_components/cardrow';
 import CardRowSkeleton from './_components/cardrowskeleton';
 import RefreshButton from './_components/refreshbutton';
-import LoginButton from './_components/loginbutton';
-import { getSession } from './_lib/sessionhandler';
-import LogoutButton from './_components/logoutbutton';
+import AccountAction from './_components/accountaction';
 
-export default async function Home() {
-  const session = await getSession();
-
+export default function Home() {
   return (
     <div>
       <div className="flex justify-center">
@@ -54,7 +50,9 @@ export default async function Home() {
       <div className="flex flex-row gap-2 justify-center">
         <StyledLink href="/" label="Home" />
         <RefreshButton />
-        {!session ? <LoginButton /> : <div className="flex flex-row gap-2"><LogoutButton /> {session.user.name}</div>}
+        <Suspense fallback={<div>Loading...</div>}>
+          <AccountAction />
+        </Suspense>
       </div>
     </div>
   );
