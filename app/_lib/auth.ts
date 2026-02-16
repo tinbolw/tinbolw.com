4857f3ad986bc398c66ee05bb3f8cd3e9df591e0
Thomas Van <tinbolw@gmail.com>
2026-02-15 21:38:12 -0800

    explicitly set betterAuth baseURL

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 
 export const auth = betterAuth({
+  baseURL: "https://tinbolw.com",
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
