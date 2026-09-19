82b2080883ea7f60c7f35731454baf608af99d7c
Thomas Van <tinbolw@gmail.com>
2026-09-18 17:27:30 -0700

    feat(layout): add responsive resizing for smaller screens

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="flex flex-col min-h-screen items-center justify-center font-sans">
-          <main className="bg-zinc-800 p-3 rounded-lg max-w-[40vw]">
+          <main className="bg-zinc-800 p-3 max-md:rounded-none rounded-lg 2xl:max-w-[40vw] xl:max-w-154 md:max-w-154 w-full max-w-full transition-all duration-300 ease-in-out">
             {children}
           </main>
         </div>
